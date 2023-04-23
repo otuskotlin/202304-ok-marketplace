99fd67ee7ed84a4e0bea0bf833137bb7e234dbb7
evgeniyN <enji81@gmail.com>
2023-04-23 16:22:43 +0300

    ok-marketplace-biz - stub
    (cherry picked from commit 0c51d512cc0421086229a46031a8da483150dc35)

---
+++ ok-marketplace-stubs/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    jvm {}
+    macosX64 {}
+    linuxX64 {}
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+
+                implementation(project(":ok-marketplace-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+    }
+}
