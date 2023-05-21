09f587e80b0387740deb0b18f8908632f636f8c9
evgeniyN <enji81@gmail.com>
2023-05-21 11:43:49 +0300

    disable demo code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,10 +7,10 @@ pluginManagement {
     }
 }
 
-include("m1l1-hello")
-include("m1l2-basic")
-include("m1l3-oop")
-include("m1l4-dsl")
-include("m1l5-coroutines")
-include("m1l6-flows-and-channels")
-include("m1l7-kmp")
+//include("m1l1-hello")
+//include("m1l2-basic")
+//include("m1l3-oop")
+//include("m1l4-dsl")
+//include("m1l5-coroutines")
+//include("m1l6-flows-and-channels")
+//include("m1l7-kmp")
