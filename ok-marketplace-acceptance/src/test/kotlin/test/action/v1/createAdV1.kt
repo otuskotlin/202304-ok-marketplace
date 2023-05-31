a58aca33e0953e7ec23066100482dba967b18f95
evgeniyN <enji81@gmail.com>
2023-05-31 12:55:01 +0300

    M4L4-s1 Реализация логики стабов + правки тестов

--- ok-marketplace-acceptance/src/test/kotlin/test/action/v1/createAdV1.kt
+++ ok-marketplace-acceptance/src/test/kotlin/test/action/v1/createAdV1.kt
@@ -4,12 +4,19 @@ import io.kotest.assertions.asClue
 import io.kotest.assertions.withClue
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import ru.otus.otuskotlin.marketplace.api.v1.models.*
 import ru.otus.otuskotlin.marketplace.blackbox.fixture.client.Client
 
 suspend fun Client.createAd(ad: AdCreateObject = someCreateAd): AdResponseObject = createAd(ad) {
     it should haveSuccessResult
-    it.ad shouldBe adStub
+    it.ad shouldNotBe null
+    it.ad?.apply {
+        title shouldBe ad.title
+        description shouldBe ad.description
+        adType shouldBe ad.adType
+        visibility shouldBe ad.visibility
+    }
     it.ad!!
 }
 
