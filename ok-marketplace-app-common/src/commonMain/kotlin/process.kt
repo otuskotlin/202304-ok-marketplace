4841e9f6bed26dc5eb0f1dac6f23a3f2c9a07e02
evgeniyN <enji81@gmail.com>
2023-06-05 18:57:01 +0300

    M4L5-s1 validation in biz

--- ok-marketplace-app-common/src/commonMain/kotlin/process.kt
+++ ok-marketplace-app-common/src/commonMain/kotlin/process.kt
@@ -3,6 +3,9 @@ package ru.otus.otuskotlin.marketplace.app.common
 import kotlinx.datetime.Clock
 import ru.otus.otuskotlin.marketplace.biz.MkplAdProcessor
 import ru.otus.otuskotlin.marketplace.common.MkplContext
+import ru.otus.otuskotlin.marketplace.common.helpers.asMkplError
+import ru.otus.otuskotlin.marketplace.common.helpers.fail
+import ru.otus.otuskotlin.marketplace.common.models.MkplCommand
 import ru.otus.otuskotlin.marketplace.logging.common.IMpLogWrapper
 
 suspend fun <T> MkplAdProcessor.process(
@@ -11,21 +14,39 @@ suspend fun <T> MkplAdProcessor.process(
     fromTransport: suspend MkplContext.() -> Unit,
     sendResponse: suspend MkplContext.() -> T,
     toLog: MkplContext.(logId: String) -> Any): T {
+    var command = MkplCommand.NONE
+    return try {
+        val ctx = MkplContext(
+            timeStart = Clock.System.now(),
+        )
 
-    val ctx = MkplContext(
-        timeStart = Clock.System.now(),
-    )
-    return logger.doWithLogging(id = logId) {
-        ctx.fromTransport()
-        logger.info(
-            msg = "${ctx.command} request is got",
-            data = ctx.toLog("${logId}-got")
-        )
-        exec(ctx)
-        logger.info(
-            msg = "${ctx.command} request is handled",
-            data = ctx.toLog("${logId}-handled")
-        )
-        ctx.sendResponse()
+        logger.doWithLogging(id = logId) {
+            ctx.fromTransport()
+            command = ctx.command
+
+            logger.info(
+                msg = "$command request is got",
+                data = ctx.toLog("${logId}-got")
+            )
+            exec(ctx)
+            logger.info(
+                msg = "$command request is handled",
+                data = ctx.toLog("${logId}-handled")
+            )
+            ctx.sendResponse()
+        }
+    } catch (e: Throwable) {
+        logger.doWithLogging(id = "${logId}-failure") {
+            logger.error(msg = "$command handling failed")
+
+            val ctx = MkplContext(
+                timeStart = Clock.System.now(),
+                command = command
+            )
+
+            ctx.fail(e.asMkplError())
+            exec(ctx)
+            sendResponse(ctx)
+        }
     }
 }
