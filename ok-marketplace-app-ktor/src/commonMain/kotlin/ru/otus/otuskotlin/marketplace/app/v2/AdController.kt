fc97a10df350a390b6e09a03e2f0ab4e99f17ccb
BarracudaPff <mrm.kikll@gmail.com>
2023-07-14 12:19:04 +0200

    [m3l2] replace ktor v2 mapping with receive/respond

--- ok-marketplace-app-ktor/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/app/v2/AdController.kt
+++ ok-marketplace-app-ktor/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/app/v2/AdController.kt
@@ -3,50 +3,47 @@ package ru.otus.otuskotlin.marketplace.app.v2
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import ru.otus.otuskotlin.marketplace.api.v2.apiV2Mapper
 import ru.otus.otuskotlin.marketplace.api.v2.models.*
 import ru.otus.otuskotlin.marketplace.biz.MkplAdProcessor
 import ru.otus.otuskotlin.marketplace.common.MkplContext
 import ru.otus.otuskotlin.marketplace.mappers.v2.*
 
 suspend fun ApplicationCall.createAd(processor: MkplAdProcessor) {
-    val request = apiV2Mapper.decodeFromString<AdCreateRequest>(receiveText())
+    val request = receive<AdCreateRequest>()
     val context = MkplContext()
     context.fromTransport(request)
     processor.exec(context)
-    respond(apiV2Mapper.encodeToString(context.toTransportCreate()))
+    respond(context.toTransportCreate())
 }
 
 suspend fun ApplicationCall.readAd(processor: MkplAdProcessor) {
-    val request = apiV2Mapper.decodeFromString<AdReadRequest>(receiveText())
+    val request = receive<AdReadRequest>()
     val context = MkplContext()
     context.fromTransport(request)
     processor.exec(context)
-    respond(apiV2Mapper.encodeToString(context.toTransportRead()))
+    respond(context.toTransportRead())
 }
 
 suspend fun ApplicationCall.updateAd(processor: MkplAdProcessor) {
-    val request = apiV2Mapper.decodeFromString<AdUpdateRequest>(receiveText())
+    val request = receive<AdUpdateRequest>()
     val context = MkplContext()
     context.fromTransport(request)
     processor.exec(context)
-    respond(apiV2Mapper.encodeToString(context.toTransportUpdate()))
+    respond(context.toTransportUpdate())
 }
 
 suspend fun ApplicationCall.deleteAd(processor: MkplAdProcessor) {
-    val request = apiV2Mapper.decodeFromString<AdDeleteRequest>(receiveText())
+    val request = receive<AdDeleteRequest>()
     val context = MkplContext()
     context.fromTransport(request)
     processor.exec(context)
-    respond(apiV2Mapper.encodeToString(context.toTransportDelete()))
+    respond(context.toTransportDelete())
 }
 
 suspend fun ApplicationCall.searchAd(processor: MkplAdProcessor) {
-    val request = apiV2Mapper.decodeFromString<AdSearchRequest>(receiveText())
+    val request = receive<AdSearchRequest>()
     val context = MkplContext()
     context.fromTransport(request)
     processor.exec(context)
-    respond(apiV2Mapper.encodeToString(context.toTransportSearch()))
+    respond(context.toTransportSearch())
 }
