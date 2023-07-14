8f075cd724560023c1737d604012e571ac096c9b
BarracudaPff <mrm.kikll@gmail.com>
2023-07-14 12:19:04 +0200

    [m3l2] replace ktor v2 mapping with receive/respond

--- ok-marketplace-app-ktor/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/app/v2/OfferController.kt
+++ ok-marketplace-app-ktor/src/commonMain/kotlin/ru/otus/otuskotlin/marketplace/app/v2/OfferController.kt
@@ -3,9 +3,6 @@ package ru.otus.otuskotlin.marketplace.app.v2
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
-import ru.otus.otuskotlin.marketplace.api.v2.apiV2Mapper
 import ru.otus.otuskotlin.marketplace.api.v2.models.AdOffersRequest
 import ru.otus.otuskotlin.marketplace.biz.MkplAdProcessor
 import ru.otus.otuskotlin.marketplace.common.MkplContext
@@ -13,9 +10,9 @@ import ru.otus.otuskotlin.marketplace.mappers.v2.fromTransport
 import ru.otus.otuskotlin.marketplace.mappers.v2.toTransportOffers
 
 suspend fun ApplicationCall.offersAd(processor: MkplAdProcessor) {
-    val request = apiV2Mapper.decodeFromString<AdOffersRequest>(receiveText())
+    val request = receive<AdOffersRequest>()
     val context = MkplContext()
     context.fromTransport(request)
     processor.exec(context)
-    respond(apiV2Mapper.encodeToString(context.toTransportOffers()))
+    respond(context.toTransportOffers())
 }
