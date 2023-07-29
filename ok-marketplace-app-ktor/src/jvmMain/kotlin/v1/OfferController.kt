otuskotlin/202302-ok-marketplace

M4L5-s0 adapt ktor to MkplAdProcessor.process

ok-marketplace-app-ktor/src/jvmMain/kotlin/v1/OfferController.kt
@@ -1,19 +1,11 @@
 package ru.otus.otuskotlin.marketplace.app.v1
 
 import io.ktor.server.application.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
 import ru.otus.otuskotlin.marketplace.api.v1.models.AdOffersRequest
+import ru.otus.otuskotlin.marketplace.api.v1.models.AdOffersResponse
 import ru.otus.otuskotlin.marketplace.app.MkplAppSettings
-import ru.otus.otuskotlin.marketplace.common.MkplContext
-import ru.otus.otuskotlin.marketplace.mappers.v1.fromTransport
-import ru.otus.otuskotlin.marketplace.mappers.v1.toTransportOffers
+import ru.otus.otuskotlin.marketplace.common.models.MkplCommand
+import ru.otus.otuskotlin.marketplace.logging.common.IMpLogWrapper
 
-suspend fun ApplicationCall.offersAd(appSettings: MkplAppSettings) {
-    val processor = appSettings.processor
-    val request = receive<AdOffersRequest>()
-    val context = MkplContext()
-    context.fromTransport(request)
-    processor.exec(context)
-    respond(context.toTransportOffers())
-}
+suspend fun ApplicationCall.offersAd(appSettings: MkplAppSettings, logger: IMpLogWrapper) =
+    processV1<AdOffersRequest, AdOffersResponse>(appSettings, logger, "ad-offers", MkplCommand.OFFERS)

otuskotlin/202304-ok-marketplace

ok-marketplace-app-ktor/src/jvmMain/kotlin/v1/OfferController.kt
@@ -1,16 +1,10 @@
 package ru.otus.otuskotlin.marketplace.app.v1
 
 import io.ktor.server.application.*
-import io.ktor.server.request.*
 import ru.otus.otuskotlin.marketplace.api.v1.models.AdOffersRequest
 import ru.otus.otuskotlin.marketplace.api.v1.models.AdOffersResponse
-import ru.otus.otuskotlin.marketplace.app.MkplAppSettings
-import kotlin.reflect.KClass
+import ru.otus.otuskotlin.marketplace.app.common.MkplAppSettings
+import ru.otus.otuskotlin.marketplace.app.v2.offersAd
 
-private val clazzOffers: KClass<*> = ApplicationCall::offersAd::class
-suspend fun ApplicationCall.offersAd(appSettings: MkplAppSettings) = processV1<AdOffersRequest, AdOffersResponse>(
-    processor = appSettings.processor,
-    request = receive<AdOffersRequest>(),
-    logger = appSettings.logger.logger(clazzOffers),
-    logId = "offers",
-)
+suspend fun ApplicationCall.offersAd(appSettings: MkplAppSettings) : Unit =
+    processV1<AdOffersRequest, AdOffersResponse>(appSettings, ApplicationCall::offersAd::class, "ad-offers")