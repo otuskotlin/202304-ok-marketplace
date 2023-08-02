4080d74f2eeab8fdb7a428d1c5eaf7d7e7f6adf4
Sergey Okatov <sokatov@gmail.com>
2023-08-02 10:21:33 +0500

    m4l1 ktor, app-common

--- ok-marketplace-acceptance/src/test/kotlin/test/AccRabbitTest.kt
+++ ok-marketplace-acceptance/src/test/kotlin/test/AccRabbitTest.kt
@@ -1,9 +1,11 @@
 package ru.otus.otuskotlin.marketplace.blackbox.test
 
+import io.kotest.core.annotation.Ignored
 import ru.otus.otuskotlin.marketplace.blackbox.docker.RabbitDockerCompose
 import ru.otus.otuskotlin.marketplace.blackbox.fixture.BaseFunSpec
 import ru.otus.otuskotlin.marketplace.blackbox.fixture.client.RabbitClient
 
+@Ignored
 class AccRabbitTest : BaseFunSpec(RabbitDockerCompose, {
     val client = RabbitClient(RabbitDockerCompose)
 
--- ok-marketplace-app-ktor/src/commonMain/kotlin/plugins/InitAppSettings.kt
+++ ok-marketplace-app-ktor/src/commonMain/kotlin/plugins/InitAppSettings.kt
@@ -3,12 +3,16 @@ package ru.otus.otuskotlin.marketplace.app.plugins
 import io.ktor.server.application.*
 import ru.otus.otuskotlin.marketplace.app.common.MkplAppSettings
 import ru.otus.otuskotlin.marketplace.biz.MkplAdProcessor
+import ru.otus.otuskotlin.marketplace.common.MkplCorSettings
 import ru.otus.otuskotlin.marketplace.logging.common.MpLoggerProvider
 
 fun Application.initAppSettings(): MkplAppSettings {
+    val corSettings = MkplCorSettings(
+        loggerProvider = getLoggerProviderConf(),
+    )
     return MkplAppSettings(
         appUrls = environment.config.propertyOrNull("ktor.urls")?.getList() ?: emptyList(),
-        processor = MkplAdProcessor(),
+        processor = MkplAdProcessor(corSettings),
         logger = getLoggerProviderConf(),
     )
 }
